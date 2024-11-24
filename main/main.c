33296391a961c84ec2aed446d03cd9bb3b721209
Jan Tomka <jan@otec.com.au>
2024-11-24 18:32:38 +1100

    Cleanup, break out game logic

--- main/main.c
+++ main/main.c
@@ -13,30 +13,27 @@
 
 //#include "ic74hc595.h"
 
+#define min(a, b) ((a) < (b) ? a : b)
+
 #define BUTTON_0_PIN GPIO_NUM_2
 #define BUTTON_1_PIN GPIO_NUM_4
 #define BUTTON_2_PIN GPIO_NUM_6
+
+button_t buttons[3];
 
 #define MATRIX_SIGNAL_PIN GPIO_NUM_12
 #define MATRIX_CLOCK_PIN GPIO_NUM_10
 #define MATRIX_LATCH_PIN GPIO_NUM_8
 
-button_t buttons[] = {
-        { .pin = BUTTON_0_PIN },
-        { .pin = BUTTON_1_PIN },
-        { .pin = BUTTON_2_PIN }
-};
+#define MATRIX_REFRESH_RATE 60
 
-matrix_t matrix = {
-        .clock_pin = MATRIX_CLOCK_PIN,
-        .signal_pin = MATRIX_SIGNAL_PIN,
-        .latch_pin = MATRIX_LATCH_PIN
-};
+static matrix_t matrix;
 
-metronome_t metronome = {
-        .clock = 0,
-        .tick_len = 10
-};
+typedef struct {
+        metronome_t metronome;
+} game_t;
+
+static game_t game;
 
 bool button_event_observer(void *data)
 {
@@ -50,21 +47,6 @@ bool button_event_observer(void *data)
         return button_status_has_changed(button);
 }
 
-void shift_two_bytes(uint8_t two[2])
-{
-        uint16_t one = (uint16_t) two[0] << 8 | two[1];
-
-        if (one == 0) {
-                one = 1U;
-        } else {
-                one = one << 1;
-                if (one == 0)
-                        one = 1U;
-        }
-        two[0] = one >> 8;
-        two[1] = one & 0xffU;
-}
-
 void button_event_handler(void *data)
 {
         button_t *button;
@@ -74,58 +56,16 @@ void button_event_handler(void *data)
 
         button = (button_t *) data;
 
-        static uint8_t column[2] = { 0U, 0U };
-        static uint8_t row[2] = { 0U, 0U };
-
         if (button->status)
                 return;
-
-        if (button->pin == BUTTON_1_PIN) {
-                shift_two_bytes(column);
-        }
-
-        if (button->pin == BUTTON_2_PIN) {
-                shift_two_bytes(row);
-        }
-
-        ic74hc595_send8bits(matrix.ic74hc595, row[0]);
-        ic74hc595_send8bits(matrix.ic74hc595, row[1]);
-        ic74hc595_send8bits(matrix.ic74hc595, column[0]);
-        ic74hc595_send8bits(matrix.ic74hc595, column[1]);
-        ic74hc595_latch(matrix.ic74hc595);
-}
-
-bool metronome_event_observer(void *data)
-{
-        metronome_t *metronome;
-
-        if (data == NULL)
-                return false;
-
-        metronome = (metronome_t *) data;
-
-        return metronome_status_update(metronome);
-}
-
-void metronome_event_handler(void *data)
-{
-        usleep(10000);
-}
-
-void setup_metronome()
-{
-        metronome_init(&metronome);
-
-        event_type_t metronome_event_type = {
-                .observer = &metronome_event_observer,
-                .handler = &metronome_event_handler,
-                .data = &metronome
-        };
-        event_type_register(metronome_event_type);
 }
 
 void setup_buttons()
 {
+        buttons[0].pin = BUTTON_0_PIN;
+        buttons[1].pin = BUTTON_1_PIN;
+        buttons[2].pin = BUTTON_2_PIN;
+
         for (int i = 0; i < sizeof(buttons) / sizeof(buttons[0]); i++) {
                 button_init(&buttons[i]);
 
@@ -136,11 +76,6 @@ void setup_buttons()
                 };
                 event_type_register(button_event_type);
         }
-}
-
-bool matrix_event_observer(void *data)
-{
-        return true;
 }
 
 void matrix_event_handler(void *data)
@@ -157,23 +92,84 @@ void matrix_event_handler(void *data)
 
 void setup_matrix()
 {
+        matrix.clock_pin = MATRIX_CLOCK_PIN;
+        matrix.signal_pin = MATRIX_SIGNAL_PIN;
+        matrix.latch_pin = MATRIX_LATCH_PIN;
+        matrix.rotate = MATRIX_ROTATE_0;
+
         matrix_init(&matrix);
 
         matrix_empty(&matrix);
 
         event_type_t matrix_event_type = {
-                .observer = &matrix_event_observer,
+                .observer = &event_true,
                 .handler = &matrix_event_handler,
                 .data = &matrix
         };
         event_type_register(matrix_event_type);
 }
 
+bool game_event_observer(void *data)
+{
+        if (data == NULL)
+                return false;
+
+        game_t *game = (game_t *) data;
+
+        return metronome_check(&game->metronome);
+}
+
+void game_event_handler(void *data)
+{
+        static int c = 0, r = 0, c_dir = 1, r_dir = 1;
+
+        if (c < 0) {
+                c = 0;
+                c_dir = 1;
+        }
+
+        if (c >= MATRIX_COLUMNS) {
+                c = MATRIX_COLUMNS - 1;
+                c_dir = -1;
+        }
+
+        if (r < 0) {
+                r = 0;
+                r_dir = 1;
+                c += c_dir;
+        }
+
+        if (r >= MATRIX_ROWS) {
+                r = MATRIX_ROWS - 1;
+                r_dir = -1;
+                c += c_dir;
+        }
+        
+        matrix_empty(&matrix);
+        matrix_dot_add(&matrix, c, r);
+
+        r += r_dir;
+}
+
+void setup_game()
+{
+        game.metronome.tick_len = 25;
+        metronome_init(&game.metronome);
+
+        event_type_t game_event_type = {
+                .observer = &game_event_observer,
+                .handler = &game_event_handler,
+                .data = &game
+        };
+        event_type_register(game_event_type);
+}
+
+
 void app_main(void)
 {
         setup_buttons();
         setup_matrix();
-        setup_metronome();
+        setup_game();
 
         event_loop();
 }
