cc60a2a8c459251629de33c06269bb0dfe9e27d8
Jan Tomka <jan@otec.com.au>
2024-11-24 12:24:27 +1100

    Complete matrix device communication

--- main/matrix.c
+++ main/matrix.c
@@ -1,22 +1,114 @@
-#include "driver/gpio.h"
+#include <stdlib.h>
+#include <unistd.h>
 
 #include "matrix.h"
+
+const uint32_t matrix_columns[] = {
+        0x00001000UL, 0x10000000UL, 0x00080000UL, 0x00000200UL,
+        0x08000000UL, 0x00000400UL, 0x00000800UL, 0x00000100UL,
+        0x00000020UL, 0x40000000UL, 0x80000000UL, 0x00000008UL,
+        0x20000000UL, 0x00000004UL, 0x00000080UL, 0x00000001UL
+};
+
+const uint32_t matrix_rows[] = {
+        0x00000010UL, 0x00000040UL, 0x00004000UL, 0x00000002UL,
+        0x00002000UL, 0x00008000UL, 0x04000000UL, 0x02000000UL,
+        0x00020000UL, 0x00010000UL, 0x00800000UL, 0x00040000UL,
+        0x00200000UL, 0x00400000UL, 0x01000000UL, 0x00100000UL
+};
 
 void matrix_init(matrix_t *matrix)
 {
         if (matrix == NULL)
                 return;
 
-        const gpio_num_t matrix_pins[] = {
-                matrix->signal_pin,
-                matrix->clock_pin,
-                matrix->latch_pin
-        };
+        matrix->ic74hc595 = malloc(sizeof(ic74hc595_t));
 
-        for (int i = 0; i < sizeof(matrix_pins) / sizeof(matrix_pins[0]); i++) {
-                gpio_set_direction(matrix_pins[i], GPIO_MODE_OUTPUT);
-                gpio_set_pull_mode(matrix_pins[i], GPIO_PULLDOWN_ONLY);
-                gpio_set_level(matrix_pins[i], 0);
+        matrix->ic74hc595->signal_pin = matrix->signal_pin;
+        matrix->ic74hc595->clock_pin = matrix->clock_pin;
+        matrix->ic74hc595->latch_pin = matrix->latch_pin;
+        matrix->ic74hc595->send_mode = IC74HC595_SEND_MODE_MSB_FIRST;
+
+        ic74hc595_init(matrix->ic74hc595);
+
+        matrix_empty(matrix);
+}
+
+void matrix_dot_set(matrix_t *matrix, int column, int row, int value)
+{
+        if (matrix == NULL)
+                return;
+
+        if (column < 0 || column >= MATRIX_COLUMNS)
+                return;
+
+        if (row < 0 || row >= MATRIX_ROWS)
+                return;
+
+        matrix->content[column][row] = value ? 1 : 0;
+}
+
+void matrix_dot_add(matrix_t *matrix, int column, int row)
+{
+        if (matrix == NULL)
+                return;
+
+        if (column < 0 || column >= MATRIX_COLUMNS)
+                return;
+
+        if (row < 0 || row >= MATRIX_ROWS)
+                return;
+
+        matrix_dot_set(matrix, column, row, 1);
+}
+
+void matrix_dot_remove(matrix_t *matrix, int column, int row)
+{
+        if (matrix == NULL)
+                return;
+
+        if (column < 0 || column >= MATRIX_COLUMNS)
+                return;
+
+        if (row < 0 || row >= MATRIX_ROWS)
+                return;
+
+        matrix_dot_set(matrix, column, row, 0);
+}
+
+void matrix_empty(matrix_t *matrix)
+{
+        if (matrix == NULL)
+                return;
+
+        for (int i = 0; i < MATRIX_COLUMNS; i++) {
+                for (int j = 0; j < MATRIX_ROWS; j++) {
+                        matrix->content[i][j] = 0;
+                }
+        }
+}
+
+void matrix_refresh(matrix_t *matrix)
+{
+        if (matrix == NULL)
+                return;
+
+        for (int i = 0; i < MATRIX_COLUMNS; i++) {
+                uint32_t bits;
+
+                bits = matrix_columns[i];
+                for (int j = 0; j < MATRIX_ROWS; j++) {
+                        if (matrix->content[i][j]) {
+                                bits = bits & ~matrix_rows[j];
+                        } else {
+                                bits = bits | matrix_rows[j];
+                        }
+                }
+
+                for (int k = 4; k > 0; k--) {
+                        ic74hc595_send8bits(matrix->ic74hc595, ((uint8_t *) &bits)[k - 1]);
+                }
+                ic74hc595_latch(matrix->ic74hc595);
         }
 }
 
--- main/matrix.h
+++ main/matrix.h
@@ -1,18 +1,33 @@
 #include "driver/gpio.h"
 
+#include "ic74hc595.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+#define MATRIX_COLUMNS 16
+#define MATRIX_ROWS 16
 
 typedef struct {
         gpio_num_t signal_pin;
         gpio_num_t clock_pin;
         gpio_num_t latch_pin;
-        int width;
-        int height;
+
+        ic74hc595_t *ic74hc595;
+
+        uint8_t content[MATRIX_COLUMNS][MATRIX_ROWS];
 } matrix_t;
 
 extern void matrix_init(matrix_t *matrix);
+
+extern void matrix_dot_add(matrix_t *matrix, int column, int row);
+
+extern void matrix_dot_remove(matrix_t *matrix, int column, int row);
+
+extern void matrix_empty(matrix_t *matrix);
+
+void matrix_refresh(matrix_t *matrix);
 
 #ifdef __cplusplus
 }
