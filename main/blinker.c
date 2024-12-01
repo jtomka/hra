364337a57ec0e99dfec329c9ae21f103c2cf5993
Jan Tomka <jan@otec.com.au>
2024-12-01 20:57:43 +1100

    Clean up code

--- main/blinker.c
+++ main/blinker.c
@@ -1,12 +1,12 @@
-
 #include "blinker.h"
 
 static inline uint16_t blinker_get_off_len(blinker_t *blinker)
 {
         uint16_t off_len;
 
-        if (blinker == NULL)
+        if (blinker == NULL) {
                 return 0;
+        }
 
         off_len = blinker->off_len;
         if (off_len == 0) {
@@ -21,12 +21,14 @@ bool blinker_check(blinker_t *blinker)
         uint16_t off_len;
         bool needs_toggle;
 
-        if (blinker == NULL)
+        if (blinker == NULL) {
                 return false;
+        }
 
         // No blinking
-        if (blinker->on_len == 0)
+        if (blinker->on_len == 0) {
                 return true;
+        }
 
         off_len = blinker_get_off_len(blinker);
         needs_toggle = ticker_check(&blinker->ticker);
